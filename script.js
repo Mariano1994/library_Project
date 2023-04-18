81376c9888e11a6ef5f0aff304a44da592dbde67
Mariano <marianocapiliku@gmail.com>
2023-04-18 13:43:54 +0100

    Setup all initial files Build the book's object
    Use Object Constructor to create a collection of books.
    using Constructor, is the recommended way to achieve this, because all
    the books has to have the same information such as title, author, pages and
    status.
    
    Choose to create a method in the prototype instead of the constructor.
    It's the recommended way to create a method for constructor function.
    it will avoid create the same method every time we create a new instance of
    the object, in this case a new book.
    
    Create a fucntion do create new book and also a function to add new books in the collection (library)

---
+++ script.js
@@ -0,0 +1,37 @@
+// BOOK's CONSTRUCTOR
+const Book = function (title, pages, author, state) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.state = state;
+};
+
+//  BOOK METHOD TO GET THE ALL BOOK INFORMATION
+Book.prototype.getInfo = function () {
+  return `${this.title} by ${this.author} has ${this.pages} pages`;
+};
+
+// FUCNTION TO CREATE A NEW BOOK
+const createNewBook = function (title, author, pages, state) {
+  // USING BOOK PROTOTYPE TO CREATE NEW BOOKS
+  const newBook = Object.create(Book.prototype);
+  newBook.title = title;
+  newBook.author = author;
+  newBook.pages = pages;
+  newBook.state = state;
+
+  return newBook;
+};
+
+const books = [];
+// FUNCTION TO ADD NEW BOOK ON THE LIBRARY
+const addNewBook = function (book) {
+  books.push(book);
+};
+
+addNewBook((createNewBook('Start With  Why', 'Simom Sneck', 234, 'Not Readed')));
+addNewBook((createNewBook('Como Fazer Amigos e Influenciar Pessoas', 'Dale Carnige', 154, ' Readed')));
+
+console.log(`You have ${books.length} books in your library`);
+console.log('===========================================');
+books.map((book, index) => console.log(`${index + 1} - ${book.getInfo()}`));
